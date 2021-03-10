9f9b4e52c78824c8c60f60d80d2ea01e8ea3e284
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-10 17:55:05 +0100

    added newspage

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
+<main>
+    @for ($i = 0; $i < 4; $i++)
+    <div class="paragraph">
+        <h2>Giusto per popolare la pagina</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur mollitia culpa repellendus velit? Ad excepturi distinctio adipisci iusto. Adipisci eius aspernatur necessitatibus labore beatae qui illo nihil distinctio fugiat soluta.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A veritatis doloremque vero. Exercitationem placeat magni laudantium itaque nisi ex, non at earum perspiciatis neque amet distinctio quas aliquam nulla mollitia.</p>
+    </div>
+    @endfor
+</main>
 @endsection

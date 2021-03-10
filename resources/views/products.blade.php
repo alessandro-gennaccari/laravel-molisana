a87549022f67d6ac33fd3f17d7916620399840fc
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-10 17:46:30 +0100

    added productspage

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-
+<main>
+    <div class="container">
+        <div class="container-card">
+            @foreach ($formati as $formato)
+            <div class="card">
+                <img src="{{ $formato['src'] }}" alt="{{ $formato['titolo'] }}">
+                <a href="#">
+                    <div class="layover">
+                        <div class="layover-text">
+                            {{ $formato['titolo'] .' - ' .$formato['tipo'] }}
+                        </div>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</main>
 @endsection

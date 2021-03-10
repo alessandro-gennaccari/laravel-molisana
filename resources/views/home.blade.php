42a2e55075e6e03b163b0e1ed6d4e0b6666db329
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-10 17:24:14 +0100

    added homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-
+<main>
+    <div class="home-foto"></div>
+    <div class="home-text">
+        <h1>Non una comune pasta</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit libero dolor dignissimos veritatis, sequi, eaque fugiat quae voluptatibus eligendi dolores qui magni ut consequuntur? Cupiditate doloremque quidem cum voluptatum!
+        </p>
+    </div>
+</main>
 @endsection

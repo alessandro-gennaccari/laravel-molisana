6613683e780132b688f0856b14d8c82c215784c7
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-11 14:01:52 +0100

    added img

--- resources/views/info_pasta.blade.php
+++ resources/views/info_pasta.blade.php
@@ -2,5 +2,8 @@
 @section('title', 'In Dettaglio' .' ' .$tipoProdotto['titolo'])
 
 @section('content')
-
+<main>
+    <img src="{{ $tipoProdotto['src-h'] }}" alt="Anteprima Pasta">
+    <img src="{{ $tipoProdotto['src-p'] }}" alt="Confezione Pasta">
+</main>
 @endsection
